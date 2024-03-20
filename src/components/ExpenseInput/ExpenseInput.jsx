c13c03ff018308fab1d5bde3e3c387925b573a9a
zouyos <hamza.benketaf@gmail.com>
2024-03-20 09:38:54 +0100

    redo project

--- src/components/ExpenseInput/ExpenseInput.jsx
+++ src/components/ExpenseInput/ExpenseInput.jsx
@@ -1,8 +1,20 @@
+import { useDispatch } from "react-redux";
 import s from "./style.module.css";
+import { addExpense } from "store/expense/expense-slice";
 
 export function ExpenseInput(props) {
+  const dispatch = useDispatch();
+
+  function submit(e) {
+    e.preventDefault();
+    const formData = new FormData(e.currentTarget);
+    const name = formData.get("name");
+    const price = formData.get("price");
+    dispatch(addExpense({ name, price }));
+  }
+
   return (
-    <form>
+    <form onSubmit={submit}>
       <div className="row justify-content-center">
         <div className="col-12 col-sm-5 col-md-4 col-lg-4 mb-2">
           <input
--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import "./global.css";
 import { App } from "./App";
-import React, { StrictMode } from "react";
+import React from "react";
 import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import { store } from "store/index";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <StrictMode>
+  <Provider store={store}>
     <App />
-  </StrictMode>
+  </Provider>
 );
