72b0f7a141a90e77fafa662ddefe59c9c9724d08
zouyos <hamza.benketaf@gmail.com>
2024-03-27 17:42:35 +0100

    css

--- src/containers/ExpenseInput/ExpenseInput.jsx
+++ src/containers/ExpenseInput/ExpenseInput.jsx
@@ -66,7 +66,7 @@ export function ExpenseInput() {
   return (
     <form onSubmit={submit}>
       <div className="row justify-content-center">
-        <div className="col-12 col-sm-5 col-md-4 col-lg-4 mb-5">
+        <div className="col-12 col-sm-5 col-md-4 col-lg-4 mb-4">
           <input
             type="text"
             className="form-control"
@@ -77,7 +77,7 @@ export function ExpenseInput() {
           />
           <FieldError msg={formErrors.name} />
         </div>
-        <div className="col-12 col-sm-2 col-md-4 col-lg-4 mb-5">
+        <div className="col-12 col-sm-2 col-md-4 col-lg-4 mb-4">
           <input
             type="number"
             step="0.01"
